294e40fd939180885c435634502ee40a160fc992
Rahul Maddineni <maddineni.rahulbabu@gmail.com>
2024-03-17 19:56:44 -0500

    Add Facebook JS SDK

--- src/App.js
+++ src/App.js
@@ -5,28 +5,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <script>
-          window.fbAsyncInit = function() {
-            FB.init({
-              appId      : '453703323652419',
-              cookie     : true,
-              xfbml      : true,
-              version    : 'v19.0'
-            });
-              
-            FB.AppEvents.logPageView();   
-              
-          };
-
-          (function(d, s, id){
-            var js, fjs = d.getElementsByTagName(s)[0];
-            if (d.getElementById(id)) {return;}
-            js = d.createElement(s); js.id = id;
-            js.src = "https://connect.facebook.net/en_US/sdk.js";
-            fjs.parentNode.insertBefore(js, fjs);
-          }(document, 'script', 'facebook-jssdk'));
-        </script>
+      <header className="App-header">        
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
