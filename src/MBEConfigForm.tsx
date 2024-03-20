9bfe04e2b1d68c39e7c56ac558c2c41705d72299
Rahul Maddineni <maddineni.rahulbabu@gmail.com>
2024-03-20 02:19:23 -0500

    Add Timezone input with React Select Searchable Component

--- src/MBEConfigForm.tsx
+++ src/MBEConfigForm.tsx
@@ -1,6 +1,7 @@
-import React, { useState, useEffect } from "react";
+import React, { useEffect } from "react";
 import { useForm } from "react-hook-form";
-import { MBEConfig } from "./types/MBEConfig.ts";
+import { MBEConfig, Timezones } from "./types/MBEConfig.ts";
+import Select from "react-select";
 
 type Props = {};
 
@@ -9,22 +10,35 @@ type FormConfigProps = {
 };
 type FormProps = MBEConfig & FormConfigProps;
 
-const MBEConfigForm: React.FC<Props> = ({}) => {
+const MBEConfigForm: React.FC<Props> = () => {
   const {
     register,
     handleSubmit,
     watch,
     setValue,
-    formState: { errors },
+    // formState: { errors },
   } = useForm<FormProps>({
     defaultValues: {
       randomExtBizID: true,
-      extras: {},
+      extras: {
+        setup: {
+          external_business_id: "",
+          timezone: "America/Los_Angeles",
+        },
+      },
     },
   });
 
   const generateRandomId = () => Math.random().toString(36).substring(2, 12);
   const isRandomExtBizIDChecked = watch("randomExtBizID");
+
+  const timezoneOptions = Timezones.map((timezone) => ({
+    value: timezone,
+    label: timezone,
+  }));
+  const handleTimezoneChange = (selectedOption) => {
+    setValue("extras.setup.timezone", selectedOption.value);
+  };
 
   useEffect(() => {
     // Set Random External Business ID when toggled on
@@ -51,6 +65,17 @@ const MBEConfigForm: React.FC<Props> = ({}) => {
           id="randomExtBizID"
           type="checkbox"
           {...register("randomExtBizID")}
+        />
+      </div>
+      <div>
+        <label htmlFor="timezone">Timezone</label>
+        <Select
+          id="timezone"
+          options={timezoneOptions}
+          defaultValue={timezoneOptions.find(
+            (option) => option.value === "America/Los_Angeles"
+          )}
+          onChange={handleTimezoneChange}
         />
       </div>
       <button type="submit">Configure MBE</button>
